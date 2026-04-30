f24dce476967d322214068cc734842ef6d7a5901
Jesse Abdul <jesse.abdul@noaa.gov>
2026-04-29 19:17:39 -1000

    bug fix

--- containerized_oracle_development_environment/deployment_scripts/container_scripts/includes/include_container_resources.sh
+++ containerized_oracle_development_environment/deployment_scripts/container_scripts/includes/include_container_resources.sh
@@ -19,7 +19,7 @@ source "${CURR_DIR}/../../config/custom_secret_config.sh"
 source "${CURR_DIR}/../../config/container_config.sh"
 
 # include the CODE core container functions
-source "${CURR_DIR}/../../../CODE_core_scripts/CODE_container_scripts.sh"
+source "${CURR_DIR}/../../../CODE_core_scripts/CODE_container_functions.sh"
 
 # include the container functions
 source "${CURR_DIR}/../functions/custom_container_functions.sh"
