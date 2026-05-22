844b370748390306d87ad17d0afc134bed8bd5ec
Jesse Abdul <jesse.abdul@noaa.gov>
2026-05-21 19:00:24 -1000

    Updated configuration

--- projects/CU/config/project_manifest_config.sh
+++ projects/CU/config/project_manifest_config.sh
@@ -4,10 +4,10 @@
 	# The elements should contain encoded values with the "|" character as the delimiter: sql path (within container)|sql script file|User Secret Name|Password Secret Name|Script Password Secrets (this can be one or more optional pipe-delimited secret names when a password is injected into the script - examples include a CREATE USER command) 
 	
 	# create the CU schema
-	DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/DSC/modules/DSC/SQL|@dev_container_setup/create_docker_schemas.sql|oracle_admin_user|oracle_pwd|cen_utils_db_password_secret")
+	DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/CU/modules/CU/SQL|@dev_container_setup/create_docker_schemas.sql|oracle_admin_user|oracle_pwd|cen_utils_db_password_secret")
 
 	# populate the CU schema
-	DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/DSC/modules/DSC/SQL|@automated_deployments/deploy_dev_container.sql|cen_utils_db_username_secret|cen_utils_db_password_secret")
+	DB_SCRIPTS_MAP+=("${BUILD_PATH}/../../projects/CU/modules/CU/SQL|@automated_deployments/deploy_dev_container.sql|cen_utils_db_username_secret|cen_utils_db_password_secret")
 
 
 	# define the array of compose files that are used by the individual projects (specify the path relative to the core/build directory
--- projects/CU/config/project_runtime_config.sh
+++ projects/CU/config/project_runtime_config.sh
@@ -2,6 +2,9 @@
 
 ##### Container Configuration Variables: #####
 
+	# define the project name, this must be unique to run more than one instance of CODE on a given container host machine, this will determine the container name and the folder name for the working copy of the repository on the server
+	COMPOSE_PROJECT_NAME=code_cen_utils
+
 	#--- Primary schema created by deployment script, used to check if the database is installed. If the APP_SCHEMA_NAME exists then do not run the database initialization processes ---
 	APP_SCHEMA_NAME=CEN_UTILS
 
